07de21497accaec2d6190f7c0c4289ffdf4ee2a5
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-08-05 09:35:53 +0100

    strict mode removed

--- src/index.js
+++ src/index.js
@@ -8,13 +8,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <AppProvider>
-      <Router>
-        <App />
-      </Router>
-    </AppProvider>
-  </React.StrictMode>
+  <AppProvider>
+    <Router>
+      <App />
+    </Router>
+  </AppProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
